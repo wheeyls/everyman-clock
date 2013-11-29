80d6679213866d05c76ec95222e4650552953d5d
Michael Wheeler <mwheeler@g2crowd.com>
2013-11-29 06:36:54 -0600

    first pages commit

---
+++ js/arcs.js
@@ -0,0 +1,51 @@
+define(['jquery', 'd3'], function ($, d3) {
+  var w = 500
+    , h = 500
+    , r = Math.min(w, h)
+    ;
+
+  function render(clock) {
+    var svg = d3.select('#clock').append('svg')
+                .attr('width', w).attr('height', h)
+      , g
+      ;
+
+    function update(data) {
+      g = svg.selectAll('path').data(data)
+          .attr('d', function (d) {
+            return d.arc(w * .45)(d);
+          })
+         ;
+
+      g.enter()
+        .append('g')
+        .append('path')
+        .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+        .style('fill', function (d, i) {
+          if (i === 0) {
+            return 'gray';
+          } else if (d.live === true) {
+            return 'red';
+          } else {
+            return 'lightblue';
+          }
+        })
+        .attr('d', function (d) { return d.arc(w * .45)(d) })
+        .on('mouseover', function (d, i) {
+          i > 0 && $(document).trigger('floater:in', [d.start.toString(), d.end.toString()]);
+        })
+        .on('mouseout', function (d, i) {
+          i > 0 && $(document).trigger('floater:out', [d.start.toString(), d.end.toString()]);
+        })
+        ;
+    }
+
+    update(clock.periods);
+
+    $(clock).on('change', function () {
+      update(clock.periods);
+    });
+  }
+
+  return render;
+});
